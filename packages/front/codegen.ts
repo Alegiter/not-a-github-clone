c82faf9920b3a254f4c222cef415a9385d718900
Vladimir Sitnik @Alegiter <vovasitnik@gmail.com>
2023-12-14 20:11:44 +0100

    fix codegen due to missed code

--- packages/front/codegen.ts
+++ packages/front/codegen.ts
@@ -1,12 +1,14 @@
 
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
+const folder = ".generated/github"
+
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://docs.github.com/public/schema.docs.graphql",
   documents: "src/shared/api/github/api.ts",
   generates: {
-    "packages/front/src/shared/api/github/__graphql__/": {
+    [`${folder}/graphql/`]: {
       preset: "client",
       plugins: [],
       config: {
